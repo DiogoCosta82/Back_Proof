f28e23bcb5e0f0ff1c4852394d379e690627c14f
DiogoCosta82 <132056422+DiogoCosta82@users.noreply.github.com>
2023-10-08 17:06:06 +0200

    reset et forgot PWD ok

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -79,8 +79,5 @@ class AuthController extends Controller {
     
 
 
-    // get the authenticated user method
-    public function user(Request $request) {
-        return new UserResource($request->user());
-    }
+ 
 }
